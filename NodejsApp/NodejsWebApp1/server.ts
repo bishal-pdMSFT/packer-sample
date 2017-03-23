6d30334d2f12b04eb4283beaa8043f5f794c0b96
Bishal Prasad <biprasad@microsoft.com>
2017-03-23 12:09:23 +0530

    sln

--- NodejsApp/NodejsWebApp1/server.ts
+++ NodejsApp/NodejsWebApp1/server.ts
@@ -3,5 +3,5 @@ import os = require('os');
 var port = process.env.port || 1337
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from ' + os.hostname());
+    res.end('Hello World from old instance' + os.hostname());
 }).listen(port);
