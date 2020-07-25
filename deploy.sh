0fbe69dc323af3a745a7987326041eb255a2d6d0
Bishal Prasad <biprasad@microsoft.com>
2020-07-25 16:01:27 +0530

    fix bash script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 script_dir=$(dirname "$0")
-echo $script_dir
-echo "$1"
+echo "Current dir: $script_dir"
+echo "first arg: $1"
 rm -rf "./$1"
 mkdir "./$1"
 cp ./dummy.tar.gz "./$1"
 
-if [ "$2" == "true" ]; then
+if $2 == true ; then
     echo "some custom error!" 1>&2
     exit 1
 fi
